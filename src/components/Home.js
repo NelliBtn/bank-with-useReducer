cf642b457295beb2c6048261330a6ebca0d87ae2
Nelli Butenko <butenkonelly93@gmail.com>
2021-12-12 16:42:46 -0500

    add comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import { useReducer } from 'react';
 
+// 'bank', function with set of instructions to manipulate the state depending on action type from dispatch
 const reducer = () => {
 
 };
 
 export default function Home() {
 
+  // initial state and dispatch function
   const [amount, dispatch] = useReducer(reducer, 500);
+
   return (
     <div>
       {amount}
