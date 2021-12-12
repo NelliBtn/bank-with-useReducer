ccf39acea6a9b5a579e1ad9071b244b2c17c2570
Nelli Butenko <butenkonelly93@gmail.com>
2021-12-12 16:45:53 -0500

    use actionCreator to create deposit function

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,19 @@ export default function Home() {
   // initial state and dispatch function
   const [amount, dispatch] = useReducer(reducer, 500);
 
+  // actionCreator function 
+  const deposit = (amount) => {
+    // add validation
+    if (!amount) {
+      console.log('The amount is not valid')
+    }
+    // create dispatch for the action (takes an object)
+    dispatch({
+      type: "DEPOSIT",
+      payload: amount
+    });
+  }
+
   return (
     <div>
       {amount}
